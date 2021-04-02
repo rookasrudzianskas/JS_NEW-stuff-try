565897ff4dacb15671b242ffb5855081540f9635
Rokas <rookas.rudzenskas@gmail.com>
2021-04-02 09:12:01 +0300

    Initial

---
+++ main.js
@@ -0,0 +1,23 @@
+const arr = [1, 2, 3, 4, 5];
+
+
+// const newArr = arr.reduce(function(acc, val, ) {
+//   return acc + val;
+// }, 0)
+// console.log(newArr);
+// const newArr = arr.map(function (val,index, arr) {
+//   return val + 5;
+// })
+// console.log(newArr);
+
+
+// const newArr = arr.map((value) => {
+//   return val + 5;
+// });
+
+
+
+
+
+
+
