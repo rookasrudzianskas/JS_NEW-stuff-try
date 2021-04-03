ce3da3333457621afecae91d2d5c4ee183803959
Rokas <rookas.rudzenskas@gmail.com>
2021-04-03 08:16:40 +0300

    Initial

--- main.js
+++ main.js
@@ -23,10 +23,114 @@ const arr = [1, 2, 3, 4, 5];
 
 
 
-const arrowF = (firstArg = "wow", secondArg = "hello") => {
-  console.log(firstArg, secondArg);
-}
+// const arrowF = (firstArg = "wow", secondArg = "hello") => {
+//   console.log(firstArg, secondArg);
+// }
+//
+// arrowF();
 
-arrowF();
+// const arr = ["thos", "as", "I", "Something"];
+//
+// const [, secondEl] = arr;
+//
+// console.log(secondEl)
+
+
+// const person = {
+//   name: "Rokas",
+//   age: 19,
+//   hobby: "hockey",
+// };
+//
+// function someName({name} = {}) {
+//   console.log(name);
+// }
+//
+// someName();
+//
+
+// const person = {
+//   name: "Rokas",
+//   age: 19,
+//   hobby: "hockey",
+// };
+//
+// const extendedPerson = {
+//   ...person,
+//   wow: "wow",
+// };
+//
+// console.log(extendedPerson);
+// const { age, name, ...newObj } = person;
+//
+// console.log(age, newObj);
+
+// const arr = ["Rplas", "Some", "As", "FDds"];
+//
+// const [firstEl, secondEl, ...newArr] = arr;
+// console.log(firstEl);
+
+// function a(firstArg, secondArg, thirdArg) {
+//   console.log();
+// }
+//
+// a(4, 5, 6, 3, 3)
+//
+// const myPromiseFunc = (num) => {
+//   return new Promise((resolve, reject) => {
+//     if(num > 5) {
+//       resolve("NUmber is greater than 5");
+//     } else {
+//       reject("number is less than 5");
+//     }
+//     // resolve("Hello");
+//     // reject("wow");
+//   });
+//
+// };
+
+
+// async function executeMyPromise() {
+//   try{
+//     const val = await myPromiseFunc(10);
+//     console.log(val);
+//
+//   } catch (err) {
+//     console.log(err);
+//   }
+//
+// }
+//
+// executeMyPromise()
+//
+// myPromiseFunc(10).then((val) => {
+//   console.log("then syntax:", val);
+//
+// }).then((newVal) => {
+//   console.log(newVal);
+// }).catch((err) => {
+//   console.log(err);
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
